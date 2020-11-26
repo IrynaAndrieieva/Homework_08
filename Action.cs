dd1af805b68861667ea12e593bb4c7b578238e05
Iryna <ortofosfatnyi@gmail.com>
2020-11-26 11:48:58 +0200

    Add in Action Inform and Warning

---
+++ Action.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+using System.Threading.Tasks;
+
+namespace Homework_08
+{
+    class Action
+    {
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public void Inform()
+        {
+            logger.Info("Start method");
+        }
+
+        public void Warning()
+        {
+            logger.Warn("Skipped logic in method");
+        }
+
+        //public void Exception()
+        //{
+        //    try
+        //    {
+        //        throw new Exception("I broke a toilet");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ex.StackTrace
+        //    }
+        //}
+
+    }
+}
