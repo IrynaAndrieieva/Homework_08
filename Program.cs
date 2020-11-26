9318adaaa0d96de87f6c53c8340ae1612346cd8c
IrynaAndrieieva <74906927+IrynaAndrieieva@users.noreply.github.com>
2020-11-26 15:12:33 +0200

    Revert "Branch"

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NLog;
 using System.Threading.Tasks;
 
 namespace Homework_08
@@ -11,16 +10,6 @@ namespace Homework_08
     {
         static void Main(string[] args)
         {
-            Starter start = new Starter();
-            start.Run();
-            Console.ReadLine();
         }
-
-  
-
     }
 }
-
-    
-
-
